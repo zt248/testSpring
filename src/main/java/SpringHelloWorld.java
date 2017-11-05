a10d0eb83497475ee01b2b72d776f4c30536c570
zhenya <ztsaryk@bk.ru>
2017-11-05 15:24:19 +0300

    commit

---
+++ src/main/java/SpringHelloWorld.java
@@ -0,0 +1,11 @@
+public class SpringHelloWorld {
+    private String helloMessage;
+
+    public void setHelloMessage(String helloMessage) {
+        this.helloMessage = helloMessage;
+    }
+
+    public void getHelloMessage() {
+        System.out.println("Hello message: " + helloMessage);
+    }
+}
---
+++ src/main/java/SpringHelloWorldRunner.java
@@ -0,0 +1,11 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringHelloWorldRunner {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("spring-hello-bean.xml");
+        SpringHelloWorld springHelloWorld = (SpringHelloWorld) context.getBean("springHelloWorld");
+        springHelloWorld.getHelloMessage();
+    }
+}
